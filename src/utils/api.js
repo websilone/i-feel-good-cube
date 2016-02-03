61715b8a23c14a647c64c147baf0ea78bb564eab
websilone <boris@louboff.com>
2016-02-03 10:24:50 +0100

    Plug in wizz

--- src/utils/api.js
+++ src/utils/api.js
@@ -26,12 +26,25 @@ const getTeam = id => {
     // return Promise.resolve(team)
 }
 
+const getUser = id => {
+    const url = `http://10.1.3.159:4567/cube/user/${id}`
+    return fetch(url)
+            .then((res) => res.json())
+
+    // return Promise.resolve(team)
+}
+
 const sendWizz = id => {
-    return Promise.resolve()
+    const url = `http://10.1.3.159:4567/cube/user/${id}/wizz`
+
+    return fetch(url, {
+        method: 'POST'
+    })
 }
 
 export {
     getTeams,
     getTeam,
+    getUser,
     sendWizz
 }
